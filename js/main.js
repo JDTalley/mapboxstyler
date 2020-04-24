23958d52789e33a9c5443d17c2c851d456ac5be1
Brian K <>
2020-04-24 16:16:09 -0400

    Map Display Working

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+mapboxgl.accessToken = 'pk.eyJ1Ijoia2V0Y2hlbTIiLCJhIjoiY2s5ZW1zcGNsMDBmODNtcGhjbDI3OWY2cCJ9.Dp0zt7PQr7To4lhw1d5ZUg';
+var map = new mapboxgl.Map({
+    container: 'map', // container id
+    style: 'mapbox://styles/mapbox/streets-v11', // stylesheet location
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
+
+var mapFunctions= {
+
+}
