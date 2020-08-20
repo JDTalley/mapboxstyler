01e71cd974f930d1f8acc44a147aa3d60201fca8
Dakota <dakota.talley@gmail.com>
2020-08-19 22:37:48 -0400

    Started adding line props.

--- js/main.js
+++ js/main.js
@@ -11,17 +11,8 @@ let map = new mapboxgl.Map({
 
 // Initiate Values
 let gType;
-
-//Point
-let pointValues = {
-    pointRadius: 5,
-    pointColor: '#000000',
-    pointBlur: 0,
-    pointOpacity: 1,
-    pointSColor: '#000000',
-    pointSOpacity: 1,
-    pointSWidth: 0
-};
+let pointValues;
+let lineValues;
 
 // Select Input div
 let inputdiv = document.querySelector('.input');
@@ -32,8 +23,9 @@ let geoTypebtn = document.querySelector('.geometry-type');
 // Select Feat divs
 let featDivs = document.querySelectorAll('.input-feat');
 
-// Select Point div
+// Select Prop divs
 let pointdiv = document.querySelector('.input-feat-point');
+let linediv = document.querySelector('.input-feat-line');
 
 // Select Point Properties
 let pointRadius = document.querySelector('.point-radius');
@@ -80,6 +72,66 @@ genJSONbtn.addEventListener('click', (e) => {
 
 // Geography Type
 function geoType(e) {
+    addSource(gType);
+    hideProps();
+    resetDefaults();
+    showProps(gType);
+
+    switch (gType) {
+        case 'Point':
+            addPoint();
+            break;
+        case 'LineString':
+            addLine();
+            break;
+    }
+}
+
+function resetDefaults() {
+    pointValues = {
+        pointRadius: 5,
+        pointColor: '#000000',
+        pointBlur: 0,
+        pointOpacity: 1,
+        pointSColor: '#000000',
+        pointSOpacity: 1,
+        pointSWidth: 0
+    };
+
+    lineValues = {
+        lineBlur: 0,
+        lineCap: 'butt',
+        lineColor: '#000000',
+        lineOpacity: 1,
+        lineWidth: 1
+    };
+}
+
+function addSource(type) {
+    // Remove Previous Layers
+    try {
+        map.removeLayer('PointLayer');
+    } catch (error) {
+        console.log('No point layer to remove');
+    }
+    try {
+        map.removeLayer('LineLayer');
+    } catch (error) {
+        console.log('No line layer to remove');
+    }
+
+    // Remove Previous Sources
+    try {
+        map.removeSource('Point');
+    } catch (error) {
+        console.log('No point source to remove');
+    }
+    try {
+        map.removeSource('LineString');
+    } catch (error) {
+        console.log('No line source to remove');
+    }
+    
     map.addSource(gType, {
         'type': 'geojson',
         'data': {
@@ -90,23 +142,33 @@ function geoType(e) {
                     'type': 'Feature',
                     'geometry': {
                         'type': gType,
-                        'coordinates': [
-                            -80.4139,
-                            37.22
-                        ]
+                        'coordinates': getCords(gType)                           
                     },
                     'properties': {
-                        'title': 'Blacksburg Point'
+                        'title': 'Blacksburg Feature'
                     }
                 }
             ]
         }
     });
+}
 
-    pointdiv.classList.remove('hidden');
-    pointdiv.style.display = 'block';
+function hideProps() {
+    pointdiv.style.display = 'none';
+    linediv.style.display = 'none';
+}
 
-    addPoint();
+function showProps(type) {
+    switch (type) {
+        case "Point":
+            pointdiv.classList.remove('hidden');
+            pointdiv.style.display = 'block';
+            break;
+        case "LineString":
+            linediv.classList.remove('hidden');
+            linediv.style.display = 'block';
+            break;
+    }
 }
 
 function addPoint() {
@@ -128,6 +190,30 @@ function addPoint() {
 
     // Set Initial Values
     setPointVals();
+}
+
+function addLine() {
+    map.addLayer({
+        'id': 'LineLayer',
+        'type': 'line',
+        'source': gType,
+        'paint': {
+            'line-blur': lineValues.lineBlur,
+            //'line-cap': lineValues.lineCap,
+            'line-color': lineValues.lineColor,
+            'line-opacity': lineValues.lineOpacity,
+            'line-width': lineValues.lineWidth
+        }
+    });
+}
+
+function getCords(type) {
+    switch (type) {
+        case 'Point':
+            return ([-80.4139, 37.22]);
+        case 'LineString':
+            return([[-80.4139, 37.22],[-80.4141, 37.23]]);
+    }
 }
 
 // Generate JSON
@@ -156,6 +242,7 @@ function generateJSON() {
             ", 'circle-stroke-width': " +
             pointValues.pointSWidth +
             "}"
+            break;
     }
 
     console.log(textArea);
@@ -230,254 +317,3 @@ function setPointVals() {
     pointSOpacity.value = pointValues.pointSOpacity;
     pointSWidth.value = pointValues.pointSWidth;
 }
-
-// Wait for map to load
-/* map.on('load', function () {
-    map.addSource('points', {
-        'type': 'geojson',
-        'data': {
-            'type': 'FeatureCollection',
-            'features': [
-                {
-                    // feature for Mapbox DC
-                    'type': 'Feature',
-                    'geometry': {
-                        'type': 'Point',
-                        'coordinates': [
-                            -80.4139,
-                            37.22
-                        ]
-                    },
-                    'properties': {
-                        'title': 'Blacksburg Point'
-                    }
-                }
-            ]
-        }
-    });
-
-    // Hide Loading Div
-    //document.querySelector('#main-load').classList.toggle("fadeout");
-}); */
-
-/* addPointButton.addEventListener('click', () => {
-    addPointButton.classList.toggle("hidden");
-
-    let fields = document.querySelectorAll('.point');
-    fields.forEach((e) => {
-        e.classList.toggle("hidden");
-    })
-
-    map.addLayer({
-        'id': 'newPointLayer',
-        'type': 'circle',
-        'source': 'points',
-        'paint': {
-            // make circles larger as the user zooms from z12 to z22
-            'circle-radius': pointRadius,
-            // color circles by ethnicity, using a match expression
-            // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
-            'circle-color': pointColor
-        }
-    });
-
-    pointName.value = "Point Layer";
-    pointRadiusButton.value = pointRadius;
-    pointColorButton.value = 'black';
-}) */
-
-/* cancelPoint.addEventListener('click', () => {
-    map.removeLayer('newPointLayer');
-
-    addPointButton.classList.toggle("hidden");
-
-    let fields = document.querySelectorAll('.point');
-    fields.forEach((e) => {
-        e.classList.toggle("hidden");
-    })
-}) */
-
-/* pointRadiusButton.addEventListener('change', (e) => {
-    pointRadius = parseInt(e.target.value);
-
-    map.removeLayer("newPointLayer");
-
-    map.addLayer({
-        'id': 'newPointLayer',
-        'type': 'circle',
-        'source': 'points',
-        'paint': {
-            // make circles larger as the user zooms from z12 to z22
-            'circle-radius': pointRadius,
-            // color circles by ethnicity, using a match expression
-            // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
-            'circle-color': pointColor
-        }
-    });
-}) */
-
-/* pointColorButton.addEventListener('change', (e) => {
-    pointColor = e.target.value;
-
-    map.removeLayer("newPointLayer");
-
-    map.addLayer({
-        'id': 'newPointLayer',
-        'type': 'circle',
-        'source': 'points',
-        'paint': {
-            // make circles larger as the user zooms from z12 to z22
-            'circle-radius': pointRadius,
-            // color circles by ethnicity, using a match expression
-            // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
-            'circle-color': pointColor
-        }
-    });
-}) */
-
-/* submitPoint.addEventListener('click', () => {
-    let pointNameVal = pointName.value;
-
-    if(pointNameVal === "newPointLayer") {
-        console.log("Reserved Name");
-    } else if(map.getLayer(pointNameVal)) {
-        console.log("Name already exists");
-    } else {
-        map.removeLayer("newPointLayer");
-
-        map.addLayer({
-            'id': pointNameVal,
-            'type': 'circle',
-            'source': 'points',
-            'paint': {
-                // make circles larger as the user zooms from z12 to z22
-                'circle-radius': pointRadius,
-                // color circles by ethnicity, using a match expression
-                // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
-                'circle-color': pointColor
-            }
-        });
-
-        pointArr.push(pointNameVal);
-
-        addPointButton.classList.toggle("hidden");
-        editPointButton.classList.toggle("hidden");
-
-        let fields = document.querySelectorAll('.point');
-        fields.forEach((e) => {
-            e.classList.toggle("hidden");
-        })
-
-        pointRadius = 10;
-        pointColor = 'black';
-    }
-}) */
-
-/* editPointButton.addEventListener('click', () => {
-    editPointButton.classList.toggle("hidden");
-    addPointButton.classList.toggle("hidden");
-
-    pointArr.forEach(layer => {
-        let input = document.createElement("input");
-        input.type = 'submit';
-        input.value = layer;
-        input.name = layer;
-        input.className = 'edit-point';
-
-        input.addEventListener('click', (e) => {
-            let edits = document.querySelectorAll('.edit-point');
-            edits.forEach(e => {
-                e.classList.toggle("hidden");
-            })
-
-            let fields = document.querySelectorAll('.point');
-            fields.forEach(e => {
-                e.classList.toggle("hidden");
-            })
-
-            let oldLayer = map.getLayer(e.target.value);
-            let oldColorR = oldLayer.paint._values["circle-color"].value.value.r;
-            let oldColorG = oldLayer.paint._values["circle-color"].value.value.g;
-            let oldColorB = oldLayer.paint._values["circle-color"].value.value.b;
-            let oldColorA = oldLayer.paint._values["circle-color"].value.value.a;
-            let oldColorHex = RGBAToHex(oldColorR, oldColorG, oldColorB);
-
-            map.removeLayer(e.target.value);
-        
-            map.addLayer({
-                'id': 'newPointLayer',
-                'type': oldLayer.type,
-                'source': 'points',
-                'paint': {
-                    // make circles larger as the user zooms from z12 to z22
-                    'circle-radius': oldLayer.paint._values["circle-radius"].value.value,
-                    // color circles by ethnicity, using a match expression
-                    // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
-                    'circle-color': oldColorHex
-                }
-            });
-        
-            pointName.value = oldLayer.id;
-            pointRadiusButton.value = oldLayer.paint._values["circle-radius"].value.value;
-            pointColorButton.value = oldColorHex;
-        })
-
-        document.querySelector('#edit').appendChild(input);
-    })
-}) */
-
-/* let RGBAToHex = (r,g,b) => {
-    r = parseInt(r).toString(16);
-    console.log(r);
-    g = parseInt(g).toString(16);
-    console.log(g);
-    b = parseInt(b).toString(16);
-    console.log(b);
-    //a = Math.round(a * 255).toString(16);
-
-    if (r.length == 1)
-        r = "0" + r;
-    if (g.length == 1)
-        g = "0" + g;
-    if (b.length == 1)
-        b = "0" + b;
-    //if (a.length == 1)
-        //a = "0" + a;
-
-    return "#" + r + g + b;
-} */
-
-// document.ready not necessary.
-/* $(document).ready(function(){
-    $('#changePoint').on("click", function(){
-        var inputRadius = parseInt($('#pointRadius').val());
-        var inputColor = $('#pointColor').val();
-
-        var pointRadius = 10;
-        var pointColor = '#000';
-
-        if ( inputRadius != null && inputRadius > 0){
-            pointRadius = inputRadius;
-        }
-        pointColor = inputColor;
-
-        // console.log(pointRadius, pointColor);
-        // console.log(typeof(inputRadius));
-
-        map.removeLayer("pointLayer");
-
-        map.addLayer({
-            'id': 'pointLayer',
-            'type': 'circle',
-            'source': 'points',
-            'paint': {
-                // make circles larger as the user zooms from z12 to z22
-                'circle-radius': pointRadius,
-                // color circles by ethnicity, using a match expression
-                // https://docs.mapbox.com/mapbox-gl-js/style-spec/#expressions-match
-                'circle-color': pointColor
-            }
-        });
-    });
-
-});  */
