b2a8f0f57330103b7407d45cae106884c2f81d71
Dakota <dakota.talley@gmail.com>
2020-08-30 21:20:00 -0400

    Added Polygons class.

---
+++ js/polygons.js
@@ -0,0 +1,32 @@
+class Polygons {
+    constructor() {
+        this.reset()
+    }
+
+    reset() {
+        this.color = '#000000'
+        this.opacity = 1
+        this.oColor = '#000000'
+    }
+
+    addPolygon() {
+        map.addLayer({
+            'id': 'PolygonLayer',
+            'type': 'fill',
+            'source': gType,
+            'paint': {
+                'fill-color': this.color,
+                'fill-opacity': this.opacity,
+                'fill-outline-color': this.color
+            }
+        });
+    
+        this.setPolygonVals();
+    }
+
+    setPolygonVals() {
+        selectors.polygonColor.value = this.color
+        selectors.polygonOpacity.value = this.opacity
+        selectors.polygonOColor.value = this.oColor
+    }
+}
